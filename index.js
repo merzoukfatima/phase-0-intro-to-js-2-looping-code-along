0b858f229644d9a5a3a782dde0b216819a84d02e
MerzoukFatimaZohra <merzoukfatimazahraa@gmail.com>
2023-07-22 20:59:50 +0100

    Assignement Completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(array){
+    let thanks=[]
+    for(let i=0;i<array.length;i++){
+        thanks.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`)
+    }
+    return thanks
+}
+
+function countDown(count){
+for(let i=count; i>=0;i--){
+    console.log(i)
+}
+}
